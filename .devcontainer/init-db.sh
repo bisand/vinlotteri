b97c78a72a3a8c114cb1e4fbeeddf1f25b1ad593
André Biseth <andre@biseth.net>
2023-01-30 11:48:51 +0100

    Added devcontainer

---
+++ .devcontainer/init-db.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo service mariadb start
+
+newUser='vinlotteri'
+newDbPassword='password'
+newDb='vinlotteri'
+host=localhost
+#host='%'
+
+# MySQL 8 and higher versions
+commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}';GRANT ALL ON \`${newDb}\`.* TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
+
+echo "${commands}" | sudo /usr/bin/mysql -u root
