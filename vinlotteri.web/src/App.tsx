dd75d2abce7567d5f12288536551a8a8ca5d5f72
André Biseth <andre@biseth.net>
2023-01-31 07:27:57 +0000

    Testing layout with root div

--- vinlotteri.web/src/App.tsx
+++ vinlotteri.web/src/App.tsx
@@ -6,7 +6,25 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <React.StrictMode>
-      <p>This is my new website and it's using Simple.css. It's really cool. If you want to use it too, you can <a href="https://simplecss.org">visit their site</a>.</p>
+      <header id="root_header">
+        <h1>Vinlotteri</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/blog">Blog</a>
+          <a href="/notes">Notes</a>
+          <a href="/guestbook">Guestbook</a>
+          <a href="/contact">Contact</a>
+        </nav>
+      </header>
+
+      <main id="root_main">
+        <p>This is my new website and it's using Simple.css. It's really cool. If you want to use it too, you can <a href="https://simplecss.org">visit their site</a>.</p>
+      </main>
+
+      <footer id="root_footer">
+        <p>Vinlotteri.com</p>
+      </footer>
     </React.StrictMode>
   );
 }
--- vinlotteri.web/src/index.tsx
+++ vinlotteri.web/src/index.tsx
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './Header';
-import Footer from './Footer';
 import { BrowserRouter, Route, Router } from 'react-router-dom';
 
-const root_header = ReactDOM.createRoot(
-  document.getElementById('root_header') as HTMLElement
-);
-root_header.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
 const root_main = ReactDOM.createRoot(
-  document.getElementById('root_main') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root_main.render(
   <React.StrictMode>
@@ -27,17 +16,6 @@ root_main.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-const root_footer = ReactDOM.createRoot(
-  document.getElementById('root_footer') as HTMLElement
-);
-root_footer.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Footer />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
